a9f5a2f9d4f7371453dd0c52a3a446d40559f18d
chschmitt <chschmitt@unomaha.edu>
2020-02-16 13:11:30 -0600

    Added h1 headers to hello world

--- Tech-Exercise-GroceryList-Schmitt/src/MyServletSchmitt.java
+++ Tech-Exercise-GroceryList-Schmitt/src/MyServletSchmitt.java
@@ -27,7 +27,7 @@ public class MyServletSchmitt extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World").append(request.getContextPath());
+		response.getWriter().append("<h1>Hello World</h1>").append(request.getContextPath());
 	}
 
 	/**
